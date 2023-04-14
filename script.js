cfa647522d03fb374d8ef2660f3365ff08d3fbbc
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-04-14 09:10:58 +0700

    Update styling navbar, responsive

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 // Sticky Header
-const header = document.querySelector('.header-wrapper');
+const header = document.querySelector(".header-wrapper");
 
-window.addEventListener('scroll',function() {
+window.addEventListener("scroll", function () {
   const scrollPos = window.scrollY;
 
   if (scrollPos > 0) {
-    header.classList.add('scroll');
+    header.classList.add("scroll");
   } else {
-    header.classList.remove('scroll');
+    header.classList.remove("scroll");
   }
 });
 
 // Event when click menu bar
-const menuBtn = document.querySelector('.menu-btn');
-const navbar = document.querySelector('.navbar');
-const closeBtn = document.querySelector('.close-btn');
+const menuBtn = document.querySelector("#menu-btn");
+const navbarMin = document.querySelector(".navbar-min");
+const closeBtn = document.querySelector(".close-btn");
+const navLink = document.querySelectorAll(".navbar-min ul li a");
+const body = document.querySelector("body");
+console.log(navLink);
 
-menuBtn.addEventListener('click', function() {
-  navbar.classList.toggle('active-navbar');
+menuBtn.addEventListener("click", function () {
+  navbarMin.classList.add("active-header");
+  body.classList.add("no-scroll");
 });
-closeBtn.addEventListener('click', function() {
-    navbar.classList.remove('active-navbar');
+closeBtn.addEventListener("click", function () {
+  navbarMin.classList.remove("active-header");
+  body.classList.remove("no-scroll");
+});
+navLink.forEach(element => {
+  element.addEventListener("click", function () {
+    navbarMin.classList.remove("active-header");
+    body.classList.remove("no-scroll");
+  });
 });
